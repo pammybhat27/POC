57bf151e64d4435ff8e37efc7921fd41755551ea
Paramanand Bhat <HCM3@area1.eurofins.local>
2018-05-28 15:42:58 +1000

    Update the Domain Model
    Adding relation membershiptype to Customer

---
+++ Migrations/201805280537437_UpdateDomainModel.cs
@@ -0,0 +1,34 @@
+namespace POC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateDomainModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.MembershipTypes",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Customers", "IsSubscribedToNewsletter", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Customers", "MembershipTypeId", c => c.Byte(nullable: false));
+            CreateIndex("dbo.Customers", "MembershipTypeId");
+            AddForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes");
+            DropIndex("dbo.Customers", new[] { "MembershipTypeId" });
+            DropColumn("dbo.Customers", "MembershipTypeId");
+            DropColumn("dbo.Customers", "IsSubscribedToNewsletter");
+            DropTable("dbo.MembershipTypes");
+        }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,5 +7,9 @@ namespace POC.Models {
     public class Customer {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
