da1ac13fd3052eedb02e31d8b66ec1174198dc05
Paramanand Bhat <HCM3@area1.eurofins.local>
2018-05-28 15:19:51 +1000

    Displaying the customers in the view

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using POC.Models;
+
+namespace POC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Name = "han"},
+                new Customer {Name = "Pammy"}
+            };
+        }
+    }
+}
