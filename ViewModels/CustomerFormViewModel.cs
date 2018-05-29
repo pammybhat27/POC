4b537fba4bb583173e129989aa094cabbb9f4b41
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-05-30 09:54:29 +1000

    Addig Title for CustomerFormViewModel to dynamically display edit or new

--- ViewModels/CustomerFormViewModel.cs
+++ ViewModels/CustomerFormViewModel.cs
@@ -9,5 +9,18 @@ namespace POC.ViewModels {
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
+
+
+        public string Title
+        {
+            get
+            {
+                if (Customer!= null && Customer.Id != 0)
+                    return "Edit Movie";
+
+                return "New Movie";
+            }
+        }
+
     }
 }
