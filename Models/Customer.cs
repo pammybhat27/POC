7b624f11c5bf3a5054ba61c7da45caab7050aaf7
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-05-30 08:48:09 +1000

    Display name change Data Annotations
    Adding data anotations to change the display name for birthdate and membershiptypeId

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -10,12 +10,15 @@ namespace POC.Models {
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         [Min18YearsIfAMember]
+        [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
 
         public MembershipType MembershipType { get; set; }
 
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
     }
 }
