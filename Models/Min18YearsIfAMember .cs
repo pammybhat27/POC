5b134a0214293d62e0e3c4ed5ac4660955cbb4ac
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-05-30 08:45:42 +1000

    Adding Custom validation to customer
    Adding custom validation to restrict only customer above 18 years to subscribre to monthly, quarterly and annual memberships

---
+++ Models/Min18YearsIfAMember .cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace POC.Models {
+    public class Min18YearsIfAMember : ValidationAttribute{
+        protected override  ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer) validationContext.ObjectInstance;
+            if(customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+
+
+            if(customer.BirthDate == null)
+                return new ValidationResult("BirthDate is required");
+
+            var age = DateTime.Now.Year - customer.BirthDate.Value.Year;
+
+
+                if(age>= 18)
+                    return ValidationResult.Success;
+
+                return new ValidationResult("Customer should be at least 18 years old to go on membership ");
+
+
+        }
+
+      
+    }
+}
