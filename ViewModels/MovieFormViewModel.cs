5ca17f8d899615e56fa12f956684d2cbe2febe8d
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-05-30 09:49:23 +1000

    Addition of Title Method in MovieFormViewModel to dynamically display edit or new movie

--- ViewModels/MovieFormViewModel.cs
+++ ViewModels/MovieFormViewModel.cs
@@ -9,5 +9,16 @@ namespace POC.ViewModels {
         public Movie Movie { get; set; }
 
         public IEnumerable<Genre> Genre { get; set; }
+
+        public string Title
+        {
+            get
+            {
+                if (Movie != null && Movie.Id != 0)
+                    return "Edit Movie";
+
+                return "New Movie";
+            }
+        }
     }
 }
