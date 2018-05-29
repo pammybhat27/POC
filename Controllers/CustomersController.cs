737d8d7a78a413d6271f1fbd0280cc9f5b58421e
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-05-30 08:10:23 +1000

    Changes To Navbar to redirect to customers and movies

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -58,8 +58,22 @@ namespace POC.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
