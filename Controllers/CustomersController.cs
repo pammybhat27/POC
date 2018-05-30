216b73e6dd200b307043dce15c51b69b8517da6d
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-05-30 11:48:51 +1000

    ModelState handled for Customer

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -58,12 +58,25 @@ namespace POC.Controllers
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            //When Adding the Model state it is invalid if no Id is specified so it will fail
+            if (customer.Id != 0)
+            {
+                if (!ModelState.IsValid)
+                {
+                    var viewModel = new CustomerFormViewModel
+                    {
+                        Customer = customer,
+                        MembershipTypes = _context.MembershipTypes.ToList()
 
+                    };
+                    return View("CustomerForm",viewModel);
+                }
 
+            }
 
+            
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
