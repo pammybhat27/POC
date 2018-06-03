77f22b8afc8e0b3925f52bb5b95137939bdbe5e1
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-06-04 09:13:09 +1000

    Adding the Movies controller in Api

--- Controllers/Api/MoviesController.cs
+++ Controllers/Api/MoviesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AutoMapper;
+using POC.Dto;
 using POC.Models;
 
 namespace POC.Controllers.Api
@@ -18,8 +20,77 @@ namespace POC.Controllers.Api
         }
 
 
+        //GET /api/Movies
+        public IEnumerable<MovieDto> GetMovies()
+        {
+            return _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+        }
+
         //GET /api/Movies/1
-       
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Movie,MovieDto>(movie));
+
+        }
+
+        //POST /api/movies
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movie = Mapper.Map<MovieDto, Movie>(movieDto);
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+
+            movieDto.Id = movie.Id;
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movieDto);
+
+
+        }
+        
+        ////PUT /api/movies/1
+        [HttpPut]
+        public void UpdateMovie(int id, MovieDto movieDto)
+        {
+            if(!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if(movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.Found);
+
+            Mapper.Map<MovieDto, Movie>(movieDto);
+            movieInDb.DateAdded = movieDto.DateAdded;
+            movieInDb.DateReleased = movieDto.DateReleased;
+            movieInDb.Name = movieDto.Name;
+            movieInDb.GenreId = movieDto.GenreId;
+            movieInDb.NumberInStock = movieDto.NumberInStock;
+
+            _context.SaveChanges();
+
+        }
+
+        //Delete api/customers/1
+        [HttpDelete]
+        public void DeleteMovie(int id) {
+            var movieInDb = _context.Movies.SingleOrDefault(c => c.Id == id);
+            if (movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+        }
+
+
 
     }
 }
