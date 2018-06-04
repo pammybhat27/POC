f3994ff65f2901afac4345beb79f80da13b9a881
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-06-04 15:39:18 +1000

    Adding Authentication and allowing anaonymous access to only home page

--- App_Start/FilterConfig.cs
+++ App_Start/FilterConfig.cs
@@ -5,6 +5,8 @@ namespace POC {
     public class FilterConfig {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
+
         }
     }
 }
