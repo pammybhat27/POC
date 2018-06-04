a17763a42ae9ce0d8b6fc564ad50bb1612b7da68
Paramanand Bhat <paramanandbhat@eurofins.com>
2018-06-05 09:39:47 +1000

    Removing the reference to Model as datatables are being used

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -36,12 +36,9 @@ namespace POC.Controllers {
         }
 
 
-        // GET: Movies
-        public ActionResult Index() {
-            var movies = _context.Movies.Include(m => m.Genre).ToList();
-
-            return View(movies);
-
+        public ViewResult Index()
+        {
+            return View();    
         }
 
 
