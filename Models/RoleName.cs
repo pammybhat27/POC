083ff86cce7bc7eff23934f4e9ffbfe2e916b9d6
Paramanand Bhat <paramanandbhat@eurofins.com>
2018-06-05 10:29:58 +1000

    Adding a static class to handle Rolenames
    Using  a static class to eliminate magic strings from the code

---
+++ Models/RoleName.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POC.Models {
+    public static class RoleName
+    {
+        public const string CanManageMovies = "CanManageMovies";
+    }
+}
