c88b1b04be16d19d62d964570956a70fcb894cb3
Paramanand Bhat <paramanandbhat@eurofins.com>
2018-06-07 09:01:22 +1000

    Adding a migration to add phone to te user table

---
+++ Migrations/201806062252548_AddingPhoneToUser.cs
@@ -0,0 +1,18 @@
+namespace POC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddingPhoneToUser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Phone", c => c.String(nullable: false, maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Phone");
+        }
+    }
+}
