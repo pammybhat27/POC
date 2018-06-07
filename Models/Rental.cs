1de53e471a403dd168123ae7c5cfa451c713f46f
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-06-08 09:59:57 +1000

    Adding Api Backend with no validation checks

--- Models/Rental.cs
+++ Models/Rental.cs
@@ -11,6 +11,9 @@ namespace POC.Models {
         [Required]
         public Customer Customer { get; set; }
 
+        [Required]
+        public Movie Movie { get; set; }
+
         public DateTime DateRented { get; set; }
 
         public DateTime? DateReturned { get; set; }
