e86b994e9a6744487ff205990403e1e1947f2d8d
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-06-08 10:14:27 +1000

    Optimistic approach for validation
    Removing the multiple validation checks

--- Controllers/Api/NewRentalsController.cs
+++ Controllers/Api/NewRentalsController.cs
@@ -23,23 +23,12 @@ namespace POC.Controllers.Api
         {
             var customer = _context.Customers.Single(
                 c => c.Id == newRental.CustomerId);
-
-            if (newRental.MovieIds.Count == 0)
-                return BadRequest("No Movie Ids have been given");
-
-
+            
             var movies = _context.Movies.Where(
                 m => newRental.MovieIds.Contains(m.Id)).ToList();
-
-
-            if (movies.Count != newRental.MovieIds.Count)
-                return BadRequest("One or more MovieIds are invalid");
-
-
-
+            
             foreach (var movie in movies)
             {
-
                 if (movie.NumberAvailable == 0)
                     return BadRequest("Movie is not available.");
 
