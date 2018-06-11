82d6ce3e91d4324ac69419506c9e26ec374e6dc0
Paramanand Bhat <paramanandbhat@eurofins.com>
2018-06-11 11:08:09 +1000

    Adding Filtering to the customer

--- Controllers/Api/CustomersController.cs
+++ Controllers/Api/CustomersController.cs
@@ -16,10 +16,17 @@ namespace POC.Controllers.Api {
         }
 
         //GET /api/customers
-        public IHttpActionResult GetCustomers()
+        public IHttpActionResult GetCustomers(string query = null)
         {
-            var customerDtos = _context.Customers.Include(c => c.MembershipType).ToList()
-                .Select(Mapper.Map<Customer, CustomerDto>);
+            var customerQuery = _context.Customers
+                .Include(c => c.MembershipType);
+
+
+            if (!String.IsNullOrWhiteSpace(query))
+                customerQuery = customerQuery.Where(c => c.Name.Contains(query));
+
+            var customerDtos    =    customerQuery.ToList()
+                  .Select(Mapper.Map<Customer, CustomerDto>);
 
             return Ok(customerDtos);
         }
